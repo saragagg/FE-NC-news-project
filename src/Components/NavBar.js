0d18d3034f00a7a4d28fc8ff20b5e19e2838a737
saragagg <saragaggino@gmail.com>
2023-03-16 14:14:25 +0000

    Starting task 09 view articles by topic

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,15 +5,18 @@ import "./NavBar.css";
 const NavBar = () => {
   const [topics, setTopics] = useState([]);
 
-  // useEffect(() => {
-  //   getTopics().then((topicsData) => {
-  //     setTopics(topicsData);
-  //   });
-  // }, []);
+  useEffect(() => {
+    getTopics().then((topicsData) => {
+      setTopics(topicsData);
+    });
+  }, []);
 
   return (
     <div className="navBar-container">
-      <button id="navBar-button"> SEE ARTICLES BY TOPICS </button>
+      {topics.map((topic) => {
+        return <Link className="navBar-button" key={topic.slug}>{topic.slug} news</Link>
+      })}
+      
     </div>
   );
 };
