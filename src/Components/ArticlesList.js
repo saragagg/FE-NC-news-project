959e3a2c81c7d13e8f7c1b214d0c49f4bd90e15b
saragagg <saragaggino@gmail.com>
2023-03-17 12:03:53 +0000

    Completing ticket 10-sort_by and order articles

--- src/Components/ArticlesList.js
+++ src/Components/ArticlesList.js
@@ -9,21 +9,21 @@ const ArticlesList = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const [searchParams, setSearchParams] = useSearchParams();
+  const topicQuery = searchParams.get("topic");
   const sortByQuery = searchParams.get("sort_by");
   const orderQuery = searchParams.get("order");
-  const topicQuery = searchParams.get('topic');
-  const setSortBy = (selectedSortBy) => {
 
-    const newSortParams = new URLSearchParams(searchParams)
+  const setSortBy = (selectedSortBy) => {
+    const newSortParams = new URLSearchParams(searchParams);
     newSortParams.set("sort_by", selectedSortBy);
     setSearchParams(newSortParams);
-  }
-  const setOrder = (selectedOrder) => {
+  };
 
-    const newOrderParams = new URLSearchParams(searchParams)
+  const setOrder = (selectedOrder) => {
+    const newOrderParams = new URLSearchParams(searchParams);
     newOrderParams.set("order", selectedOrder);
     setSearchParams(newOrderParams);
-  }
+  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -35,19 +35,27 @@ const ArticlesList = () => {
 
   return (
     <div className="articlesList">
-      <label> Sort by: 
+      <label>
+        {" "}
+        Sort by:
         <select onChange={(e) => setSortBy(e.target.value)}>
-        <option disabled value="">Sort the articles</option>
+          <option disabled value="">
+            Sort the articles
+          </option>
           <option value="created_at">Date</option>
           <option value="comment_count">Comment count</option>
           <option value="votes">Votes</option>
         </select>
       </label>
-      <label> Order: 
+      <label>
+        {" "}
+        Order:
         <select onChange={(e) => setOrder(e.target.value)}>
-        <option disabled value="" >Order the articles</option>
-        <option value="desc">Descending order</option>
-        <option value="asc">Ascending order</option>
+          <option disabled value="">
+            Order the articles
+          </option>
+          <option value="desc">Descending order</option>
+          <option value="asc">Ascending order</option>
         </select>
       </label>
       <main>
